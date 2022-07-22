838fdee02edddfe51e61964f4c96ae09cc9af041
jihyunglee <jhlee7125@gmail.com>
2022-07-22 12:50:28 +0900

    reactjs-tutorial chapter 7 lists and keys
    - Create handleDelete method to delete item
    - Add handleDelete method in FaTrashAlt font-icon
    - Click the font-icon, then delete clicked item
    - Make effect when no longer exist items, then display "Your list is empty" comment on page

--- 07_lists_and_keys/src/Content.js
+++ 07_lists_and_keys/src/Content.js
@@ -30,26 +30,42 @@ const Content = () => {
     localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   };
 
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    /* console.log(id); */
+    // local storage
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  };
+
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCheck(item.id)}
-              checked={item.checked}
-            />
-            <label
-              style={item.checked ? { textDecoration: 'line-through' } : null}
-              onDoubleClick={() => handleCheck(item.id)}
-            >
-              {item.item}
-            </label>
-            <FaTrashAlt role="button" tabIndex="0" />
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label
+                style={item.checked ? { textDecoration: 'line-through' } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0"
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )}
     </main>
   );
 };
