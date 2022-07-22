e4b7d6cf3e85b94a31b04e7a96667b00f6301987
jihyunglee <jhlee7125@gmail.com>
2022-07-22 14:48:19 +0900

    Transfer the properties App to Header component
    - Bad Case: implicitly transfer properties by just writing "props" (this notation makes ambiguous expression!)
    
    - Good Case: destructure properties to make clearly what it is transfered!

--- 08_prop_drilling/src/App.js
+++ 08_prop_drilling/src/App.js
@@ -5,7 +5,7 @@ import Footer from './Footer';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header title="Grocery List" />
       <Content />
       <Footer />
     </div>
--- 08_prop_drilling/src/Header.js
+++ 08_prop_drilling/src/Header.js
@@ -1,7 +1,23 @@
-const Header = () => {
+// bad case
+/* 
+const Header = (props) => {
   return (
     <header>
-      <h1>Groceries List</h1>
+      <h1>{props.title} List</h1>
+    </header>
+  );
+};
+
+export default Header;
+  */
+// NOTE: When you write just "props" on parameter area, then it can cause ambiguous expression, which parameters are transfered in current component
+// So, you should write specifically distructure parameters to make sure what is it!
+
+// good case
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
     </header>
   );
 };
