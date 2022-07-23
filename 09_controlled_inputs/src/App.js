c7df0e8e4971e44254a220cd498dda93306d79d5
jihyunglee <jhlee7125@gmail.com>
2022-07-24 08:04:13 +0900

    - create AddItem component
    - styling css about AddItem elements in index.css
    
    - implement AddItem component in App.js

--- 09_controlled_inputs/src/App.js
+++ 09_controlled_inputs/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 
@@ -27,22 +28,21 @@ function App() {
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
-    /* console.log(`key: ${id}`); */
-    // local storage
+
     localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
-    /* console.log(id); */
-    // local storage
+
     localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   };
 
   return (
     <div className="App">
       <Header title="Grocery List" />
+      <AddItem />
       <Content
         items={items}
         handleCheck={handleCheck}
