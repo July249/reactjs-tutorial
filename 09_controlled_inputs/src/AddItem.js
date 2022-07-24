76227e707fd229c5da0b2010cdf220e4a14f16b1
jihyunglee <jhlee7125@gmail.com>
2022-07-24 10:30:58 +0900

    adjust useRef to make initialize input and button state

--- 09_controlled_inputs/src/AddItem.js
+++ 09_controlled_inputs/src/AddItem.js
@@ -1,6 +1,9 @@
 import { FaPlus } from 'react-icons/fa';
+import { useRef } from 'react';
 
 const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef();
+
   return (
     <form className="addForm" onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
@@ -10,10 +13,15 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
         type="text"
         placeholder="Add Item"
         required
+        ref={inputRef}
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit" aria-label="Add Item">
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
         <FaPlus />
       </button>
     </form>
