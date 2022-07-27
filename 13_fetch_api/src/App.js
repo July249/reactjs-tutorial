739b77b96f4ff86666b6c438e69a211f632f1010
jihyunglee <jhlee7125@gmail.com>
2022-07-27 18:58:47 +0900

    - In during loading time, do not show error message on page - In real case, REST API do not response as fast as Local environment - Add setTimeout function to simulate loading delay at 2000msec - Set new useState, isLoading setIsLoading - If isLoading is true, "Loading Items..." paragraph shows up on page

--- 13_fetch_api/src/App.js
+++ 13_fetch_api/src/App.js
@@ -13,7 +13,9 @@ function App() {
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
   const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
+  // Below useEffect runs only first time at load items
   useEffect(() => {
     const fetchItems = async () => {
       try {
@@ -24,10 +26,14 @@ function App() {
         setFetchError(null);
       } catch (err) {
         setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
       }
     };
 
-    fetchItems();
+    setTimeout(() => {
+      fetchItems();
+    }, 2000);
   }, []);
 
   const addItem = (item) => {
@@ -67,8 +73,9 @@ function App() {
       />
       <SearchItem search={search} setSearch={setSearch} />
       <main>
+        {isLoading && <p>Loading Items...</p>}
         {fetchError && <p style={{ color: 'red' }}>{`Error: ${fetchError}`}</p>}
-        {!fetchError && (
+        {!fetchError && !isLoading && (
           <Content
             items={items.filter((item) =>
               item.item.toLowerCase().includes(search.toLowerCase())
