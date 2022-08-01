08af189090f680293917557ab83ff6476bd63e8f
jihyunglee <jhlee7125@gmail.com>
2022-08-02 00:51:03 +0900

    - Use "useParams()" method - Create delete button - Make handleDelete function - After delete post, automatically move to "Home" page (by using "useNavigate()" method)   (NOTE: No more use "useHistory" method at V6 React-Router)

--- 17_hooks_links/src/App.js
+++ 17_hooks_links/src/App.js
@@ -1,23 +1,63 @@
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import Home from './components/Home';
-import NewPost from './components/NewPost';
-import PostPage from './components/PostPage';
-import About from './components/About';
-import Missing from './components/Missing';
+import Home from './pages/Home';
+import NewPost from './pages/NewPost';
+import PostPage from './pages/PostPage';
+import About from './pages/About';
+import Missing from './pages/Missing';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([
+    /* Temporary Data */
+    {
+      id: 1,
+      title: 'My First Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 2,
+      title: 'My 2nd Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 3,
+      title: 'My 3rd Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+    {
+      id: 4,
+      title: 'My Fourth Post',
+      datetime: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
+    },
+  ]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  let navigate = useNavigate();
+
+  const handleDelete = (id) => {
+    const postsList = posts.filter((post) => post.id !== id);
+    setPosts(postsList);
+    navigate('/');
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Nav />
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home posts={posts} />} />
         <Route path="/post" element={<NewPost />} />
-        <Route path="/post/:id" element={<PostPage />} />
+        <Route
+          path="/post/:id"
+          element={<PostPage posts={posts} handleDelete={handleDelete} />}
+        />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Routes>
