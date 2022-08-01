4f9adadc55e7bbc9dec29a863f0d1c60ae18af64
jihyunglee <jhlee7125@gmail.com>
2022-08-02 07:03:13 +0900

    - Make NewPost

--- 17_hooks_links/src/pages/NewPost.js
+++ 17_hooks_links/src/pages/NewPost.js
@@ -1,7 +1,32 @@
-const NewPost = () => {
+const NewPost = ({
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+  handleSubmit,
+}) => {
   return (
-    <main>
-      <h1>NewPost</h1>
+    <main className="NewPost">
+      <h2>NewPost</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          id="postTitle"
+          type="text"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="postBody">Post:</label>
+        <textarea
+          id="postBody"
+          type="text"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 };
