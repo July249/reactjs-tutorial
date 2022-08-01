cd0f15e2d50d11df63b6b628c107f252f3ae2964
jihyunglee <jhlee7125@gmail.com>
2022-08-02 07:00:54 +0900

    - Add copyright paragraph on Footer component

--- 17_hooks_links/src/components/Footer.js
+++ 17_hooks_links/src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
+  const today = new Date();
+
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="Footer">
+      <p>Copyright &copy; {today.getFullYear()}</p>
     </footer>
   );
 };
