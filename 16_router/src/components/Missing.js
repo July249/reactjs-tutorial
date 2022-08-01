4270853907c0dcd70119fb634a9e9dfc8f71dceb
jihyunglee <jhlee7125@gmail.com>
2022-08-01 23:03:29 +0900

    - Create Header, Nav, Footer components that stay on the page even when the other components change - Create main area of the page components(Home, NewPost, PostPage, About, Missing) that occupy the main element will change - Use Route, Switch react router dom - Use useHistory (one of custom hooks)   ㄴ this hook allow us to access the browser history   ㄴ but react router does not make requests from the server again it just routes within the app   ㄴ responds faster so it improves ux - Construct the structure of components

---
+++ 16_router/src/components/Missing.js
@@ -0,0 +1,9 @@
+const Missing = () => {
+  return (
+    <main>
+      <h1>Missing</h1>
+    </main>
+  );
+};
+
+export default Missing;
---
+++ 16_router/src/components/PostPage.js
@@ -0,0 +1,9 @@
+const PostPage = () => {
+  return (
+    <main>
+      <h1>PostPage</h1>
+    </main>
+  );
+};
+
+export default PostPage;
