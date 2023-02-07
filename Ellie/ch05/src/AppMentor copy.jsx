34d2fe1e151c1d8718499f4e8c3f0c2ddb793004
jihyunglee <jhlee7125@gmail.com>
2023-02-07 16:14:45 +0900

    add: AppMentor 구현

---
+++ Ellie/ch05/src/AppMentor copy.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export default function AppMentor() {
+  const [person, setPerson] = useState({
+    name: "엘리",
+    title: "개발자",
+    mentor: {
+      name: "밥",
+      title: "시니어 개발자",
+    },
+  });
+  return (
+    <div>
+      <h1>
+        {person.name}는 {person.title}
+      </h1>
+      <p>
+        {person.name}의 멘토는 {person.mentor.name} ({person.mentor.title})
+      </p>
+      <button
+        onClick={() => {
+          const name = prompt("당신의 멘토 이름은?");
+          setPerson((person) => ({
+            ...person,
+            mentor: { ...person.mentor, name },
+          }));
+        }}
+      >
+        멘토 이름 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          const title = prompt("당신 멘토의 직책은?");
+          setPerson((person) => ({
+            ...person,
+            mentor: { ...person.mentor, title },
+          }));
+        }}
+      >
+        멘토 타이틀 바꾸기
+      </button>
+    </div>
+  );
+}
