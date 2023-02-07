a0460b223da9a9ab7496e9312f526d865e119345
jihyunglee <jhlee7125@gmail.com>
2023-02-07 19:48:29 +0900

    add: Form 구현 - 입력값 name과 email을 객체로 받아 처리하는 방법 적용

--- Ellie/ch05/src/AppForm.jsx
+++ Ellie/ch05/src/AppForm.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 
 export default function AppForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [form, setForm] = useState({ name: "", email: "" });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(form);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">이름:</label>
-      <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="text" id="name" name="name" value={form.name} onChange={handleChange} />
       <label htmlFor="email">이메일:</label>
-      <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="text" id="email" name="email" value={form.email} onChange={handleChange} />
       <button>Submit</button>
     </form>
   );
