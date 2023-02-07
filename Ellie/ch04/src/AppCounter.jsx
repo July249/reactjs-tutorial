6f047a3034ea962c5ead98220572ee47604b2d3c
jihyunglee <jhlee7125@gmail.com>
2023-02-07 15:25:40 +0900

    add: AppCounter 초기 구현 방법 주석으로 추가

--- Ellie/ch04/src/AppCounter.jsx
+++ Ellie/ch04/src/AppCounter.jsx
@@ -3,6 +3,22 @@ import "./App.css";
 import Counter from "./components/Counter";
 
 export default function AppCounter() {
+  /** 4.18 카운터 상태 끌어올리기 - 내 방법
+   * const [count1, setCount1] = useState(0);
+   * const [count2, setCount2] = useState(0);
+   * return (
+   *  <div className="container">
+   *    <div className="banner">
+   *      Total Count: {count1 + count2} {count1 + count2 > 10 ? "🔥" : "❄️"}
+   *    </div>
+   *    <div className="counters">
+   *      <Counter count={count1} setCount={setCount1} />
+   *      <Counter count={count2} setCount={setCount2} />
+   *    </div>
+   *  </div>
+   * );
+   */
+
   const [count, setCount] = useState(0);
   const handleClick = () => setCount((prev) => prev + 1);
   return (
