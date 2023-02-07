70cc1e6f131e976a2b3763c2b454066d703cf878
jihyunglee <jhlee7125@gmail.com>
2023-02-07 15:35:54 +0900

    add: AppXY 좌표 이동 구현 by using onPointerMove

---
+++ Ellie/ch05/src/AppXY copy.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./AppXY.css";
+
+export default function AppXY() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  return (
+    <div
+      className="container"
+      onPointerMove={(e) => {
+        console.log(e.clientX, e.clientY);
+        setX(e.clientX);
+        setY(e.clientY);
+      }}
+    >
+      <div className="pointer" style={{ transform: `translate(${x}px, ${y}px)` }} />
+    </div>
+  );
+}
---
+++ Ellie/ch05/src/AppXY.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./AppXY.css";
+
+export default function AppXY() {
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
+  return (
+    <div
+      className="container"
+      onPointerMove={(e) => {
+        // setPosition({ x: e.clientX, y: e.clientY });
+        // 만약 수평으로만 이동이 가능하다면?
+        // setPosition((prev) => ({ x: e.clientX, y: prev.y }));
+        setPosition((prev) => ({ ...prev, x: e.clientX }));
+      }}
+    >
+      <div className="pointer" style={{ transform: `translate(${position.x}px, ${position.y}px)` }}></div>
+    </div>
+  );
+}
