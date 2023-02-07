b30cf33b6c03d81058209a276f0cf58ce56cd97a
jihyunglee <jhlee7125@gmail.com>
2023-02-08 00:39:04 +0900

    add: AppWrap.jsx 구현 - children prop을 이용하여 구현하기

--- Ellie/ch05/src/AppWrap.jsx
+++ Ellie/ch05/src/AppWrap.jsx
@@ -3,22 +3,24 @@ import React from "react";
 export default function AppWrap() {
   return (
     <div>
-      <Navbar />
+      <Navbar>
+        <Avatar
+          image="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+          name="Bob"
+          size={200}
+        />
+        <p>안녕하세요!</p>
+      </Navbar>
+
+      <Navbar>
+        <p>안녕하세요!</p>
+      </Navbar>
     </div>
   );
 }
 
-function Navbar() {
-  return (
-    <header style={{ backgroundColor: "yellow" }}>
-      <Avatar
-        image="https://images.unsplash.com/photo-1673195006104-1f1f1facbb97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI2Nnx0b3dKWkZza3BHZ3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
-        name="Bob"
-        size={200}
-      />
-      <p>안녕하세요!</p>
-    </header>
-  );
+function Navbar({ children }) {
+  return <header style={{ backgroundColor: "yellow" }}>{children}</header>;
 }
 
 function Avatar({ image, name, size }) {
