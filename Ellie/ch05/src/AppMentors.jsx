1d249b6cd7a5bfa25fad5c7165647f92e6776dc9
jihyunglee <jhlee7125@gmail.com>
2023-02-07 16:42:21 +0900

    add: AppMentors - useReducer 사용하기

--- Ellie/ch05/src/AppMentors.jsx
+++ Ellie/ch05/src/AppMentors.jsx
@@ -1,44 +1,36 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+import personReducer from "./reducer/person-reducer";
 
 export default function AppMentor() {
-  const [person, setPerson] = useState({
-    name: "엘리",
-    title: "개발자",
-    mentors: [
-      {
-        name: "밥",
-        title: "시니어 개발자",
-      },
-      {
-        name: "제임스",
-        title: "시니어 개발자",
-      },
-    ],
-  });
+  // const [person, setPerson] = useState(initialPerson);
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
   const handleUpdate = () => {
     const prev = prompt("누구의 이름을 바꾸고 싶은가요?");
     const current = prompt("이름을 무엇으로 바꾸고 싶은가요?");
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.map((mentor) => {
-        return mentor.name === prev ? { ...mentor, name: current } : { ...mentor };
-      }),
-    }));
+    // setPerson((person) => ({
+    //   ...person,
+    //   mentors: person.mentors.map((mentor) => {
+    //     return mentor.name === prev ? { ...mentor, name: current } : { ...mentor };
+    //   }),
+    // }));
+    dispatch({ type: "updated", prev, current });
   };
   const handleAdd = () => {
     const name = prompt("추가하고 싶은 멘토의 이름은 무엇인가요?");
     const title = prompt("당신 멘토의 직책은 무엇인가요?");
-    setPerson((person) => ({
-      ...person,
-      mentors: [...person.mentors, { name, title }],
-    }));
+    // setPerson((person) => ({
+    //   ...person,
+    //   mentors: [...person.mentors, { name, title }],
+    // }));
+    dispatch({ type: "added", name, title });
   };
   const handleDelete = () => {
     const name = prompt("어떤 멘토를 삭제하고 싶은가요?");
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.filter((m) => m.name !== name),
-    }));
+    // setPerson((person) => ({
+    //   ...person,
+    //   mentors: person.mentors.filter((m) => m.name !== name),
+    // }));
+    dispatch({ type: "deleted", name });
   };
 
   return (
@@ -60,3 +52,18 @@ export default function AppMentor() {
     </div>
   );
 }
+
+const initialPerson = {
+  name: "엘리",
+  title: "개발자",
+  mentors: [
+    {
+      name: "밥",
+      title: "시니어 개발자",
+    },
+    {
+      name: "제임스",
+      title: "시니어 개발자",
+    },
+  ],
+};
---
+++ Ellie/ch05/src/reducer/person-reducer.js
@@ -0,0 +1,36 @@
+export default function personReducer(person, action) {
+  switch (action.type) {
+    case "updated": {
+      const { prev, current } = action;
+      return {
+        ...person,
+        mentors: person.mentors.map((mentor) =>
+          mentor.name === prev
+            ? {
+                ...mentor,
+                name: current,
+              }
+            : {
+                ...mentor,
+              }
+        ),
+      };
+    }
+    case "added": {
+      const { name, title } = action;
+      return {
+        ...person,
+        mentors: [...person.mentors, { name, title }],
+      };
+    }
+    case "deleted": {
+      return {
+        ...person,
+        mentors: person.mentors.filter((mentor) => mentor.name !== action.name),
+      };
+    }
+    default: {
+      throw Error(`알 수 없는 액션 타입입니다. ${action.type}`);
+    }
+  }
+}
