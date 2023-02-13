2b09be590e52b20457eda177640290c6b9f8ab68
jihyunglee <jhlee7125@gmail.com>
2023-02-13 20:11:29 +0900

    데이터 요청시 로딩, 에러 처리 기본 기법

---
+++ Ellie/ch05/src/components/Products.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("🔥 뜨끈한 데이터를 네트워크에서 받아옴");
+        setProducts(data);
+      })
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+
+    return () => {
+      console.log("🧹 깨끗하게 청소하는 일을 합니다.");
+    };
+  }, [checked]);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <>
+      <input id="checkbox" type="checkbox" value={checked} onChange={handleChange} />
+      <label htmlFor="checkbox">Show Only 🔥 Sale</label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
