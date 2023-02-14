69f8f1382e55104f7d598a53770714c688d394d8
jihyunglee <jhlee7125@gmail.com>
2023-02-14 14:49:39 +0900

    styled-components 사용 방법

--- Ellie/ch06/src/App.js
+++ Ellie/ch06/src/App.js
@@ -1,12 +1,36 @@
 import "./App.css";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
+import styled, { css } from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #3c5b69;
+  color: #b9eaff;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #009cd5;
+      color: white;
+    `}
+`;
 
 function App() {
   return (
     <>
       <Button1 />
       <Button2 />
+      <Container>
+        <Button>Normal Button</Button>
+        <Button primary>Primary Button</Button>
+      </Container>
     </>
   );
 }
