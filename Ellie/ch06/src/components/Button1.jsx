a30c566e23065bf1e511c08173871907c2f7b811
jihyunglee <jhlee7125@gmail.com>
2023-02-14 14:33:18 +0900

    기본 css 설정시 cascading으로 인한 button1 배경색 미적용 예시

---
+++ Ellie/ch06/src/components/Button1.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Button1.css";
+
+export default function Button1() {
+  return <button className="button">Button1</button>;
+}
---
+++ Ellie/ch06/src/components/Button2.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Button2.css";
+
+export default function Button2() {
+  return <button className="button">Button2</button>;
+}
