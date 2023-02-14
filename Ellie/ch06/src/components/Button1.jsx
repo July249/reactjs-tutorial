654b8e92f72ceabef5475ee8d62ea08b6f7f82ce
jihyunglee <jhlee7125@gmail.com>
2023-02-14 14:38:55 +0900

    post CSS 사용 방법

--- Ellie/ch06/src/components/Button1.jsx
+++ Ellie/ch06/src/components/Button1.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Button1.css";
+import btnStyles from "./Button1.module.css";
 
 export default function Button1() {
-  return <button className="button">Button1</button>;
+  return <button className={btnStyles.button}>Button1</button>;
 }
--- Ellie/ch06/src/components/Button2.jsx
+++ Ellie/ch06/src/components/Button2.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Button2.css";
+import btnStyles from "./Button2.module.css";
 
 export default function Button2() {
-  return <button className="button">Button2</button>;
+  return <button className={btnStyles.button}>Button2</button>;
 }
