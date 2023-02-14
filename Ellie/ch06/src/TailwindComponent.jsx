e73b16672a744e66037085a035f3303ecdea92b6
jihyunglee <jhlee7125@gmail.com>
2023-02-14 15:07:03 +0900

    tailwind 사용 방법

--- Ellie/ch06/src/TailwindComponent.jsx
+++ Ellie/ch06/src/TailwindComponent.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function TailwindComponent() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-2xl">안녕!</h1>
+      <button className="bg-blue-500 rounded-xl px-2">Button</button>
+    </div>
+  );
 }
