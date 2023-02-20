590aa2bd99c1a881863e377a68bf55e26bedd7ac
jihyunglee <jhlee7125@gmail.com>
2023-02-20 12:37:54 +0900

    feat: useState 초기값 설정시 사용하는 콜백함수로 변경

--- Ellie/todolist/src/components/TodoList/TodoList.jsx
+++ Ellie/todolist/src/components/TodoList/TodoList.jsx
@@ -4,7 +4,10 @@ import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todolist")) || []);
+  const [todos, setTodos] = useState(readTodosFromLocalStorage);
+  // useState 안에 콜백함수를 사용한 이유는 그래야 초기화하는함수를 처음에만 호출하고 값이 있는 경우에는 다시 호출하지 않기 때문이다.
+  // 이렇게 하지 않으면 값을 추가할 때마다 다시 로컬스토리지에서 값을 가져오는 불필요한 함수 호출이 지속되게 된다.
+  // const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
 
   const handleAdd = (todo) => {
     setTodos([...todos, todo]);
@@ -30,6 +33,11 @@ export default function TodoList({ filter }) {
   );
 }
 
+// 초기에만 호출되면 되는 함수임
+function readTodosFromLocalStorage() {
+  const todos = localStorage.getItem("todolist");
+  return todos ? JSON.parse(todos) : [];
+}
 function getFilteredItems(todos, filter) {
   if (filter === "all") {
     return todos;
