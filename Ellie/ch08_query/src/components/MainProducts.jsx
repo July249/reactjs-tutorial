ab52914a14ab483ccc38f379f2f99562517f56e4
jihyunglee <jhlee7125@gmail.com>
2023-02-20 15:41:35 +0900

    chore: custom hook을 여러번 호출하는 경우 발생하는 문제점

--- Ellie/ch08_query/src/components/MainProducts.jsx
+++ Ellie/ch08_query/src/components/MainProducts.jsx
@@ -5,6 +5,9 @@ export default function MainProducts() {
   const [showLeftProducts, setShowLeftProducts] = useState(true);
   const [showRightProducts, setShowRightProducts] = useState(true);
 
+  // Products 컴포넌트를 두번 호출하기 때문에 useProduct 자체도 두번 실행된다
+  // 따라서 데이터 호출 자체도 두번 되는 것이다
+  // 이는 성능상에 이슈를 야기할 수 있다
   return (
     <main className="container">
       <div>
