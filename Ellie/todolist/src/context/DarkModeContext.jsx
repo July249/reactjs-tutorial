ee52b066f5ca687043f889a8efd02c2cbba9f1f7
jihyunglee <jhlee7125@gmail.com>
2023-02-20 11:56:23 +0900

    feat: DarkMode setting값을 localStorage.theme에 저장하기

--- Ellie/todolist/src/context/DarkModeContext.jsx
+++ Ellie/todolist/src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const DarkModeContext = createContext();
 
@@ -9,6 +9,14 @@ export function DarkModeProvider({ children }) {
     updateDarkMode(!darkMode);
   };
 
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark").matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
+
   return <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>{children}</DarkModeContext.Provider>;
 }
 
@@ -16,7 +24,9 @@ export function DarkModeProvider({ children }) {
 function updateDarkMode(darkMode) {
   if (darkMode) {
     document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
   }
 }
