bb32f328b80422d368db3e6c297e2d6a7c39d9e4
jihyunglee <jhlee7125@gmail.com>
2023-02-20 14:21:40 +0900

    initial setting

---
+++ Ellie/ch08_query/src/components/Products.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import useProducts from "../hooks/useProduct";
+
+export default function Products() {
+  const [checked, setChecked] = useState(false);
+  const [loading, error, products] = useProducts({ salesOnly: checked });
+  const handleChange = () => setChecked((prev) => !prev);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <>
+      <input id="checkbox" type="checkbox" value={checked} onChange={handleChange} />
+      <label htmlFor="checkbox">Show Only Sale</label>
+      <ul>
+        {Products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ Ellie/ch08_query/src/hooks/useProduct.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export default function useProducts({ salesOnly }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+    fetch(`data/${salesOnly ? "sales_" : ""}products.json`)
+      .then((res) => res.json)
+      .then((data) => {
+        console.log("🔥 뜨끈한 데이터를 네트워크에서 받아옴");
+        setProducts(data);
+      })
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+
+    return () => {
+      console.log("Clean-Up!!");
+    };
+  }, [salesOnly]);
+
+  return [loading, error, products];
+}
