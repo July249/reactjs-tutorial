c124d7a5f37c8cc3d19d6bde7fc92342f6f1d132
jihyunglee <jhlee7125@gmail.com>
2023-02-20 12:28:39 +0900

    feat: localStorage에 데이터를 저장

--- Ellie/todolist/src/components/TodoList/TodoList.jsx
+++ Ellie/todolist/src/components/TodoList/TodoList.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: "123", text: "장보기", status: "active" },
-    { id: "124", text: "공부하기", status: "active" },
-  ]);
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todolist")) || []);
 
-  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleAdd = (todo) => {
+    setTodos([...todos, todo]);
+  };
   const handleUpdate = (updated) => setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
 
+  useEffect(() => {
+    localStorage.setItem("todolist", JSON.stringify(todos));
+  }, [todos]);
+
   const filtered = getFilteredItems(todos, filter);
+
   return (
     <section className={styles.container}>
       <ul className={styles.list}>
-        {filtered.map((item) => (
+        {filtered?.map((item) => (
           <Todo key={item.id} todo={item} onUpdate={handleUpdate} onDelete={handleDelete} />
         ))}
       </ul>
