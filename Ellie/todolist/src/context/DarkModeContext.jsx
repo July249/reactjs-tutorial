050f6624011db39b62bcc9c7b65fcaa9bf078448
jihyunglee <jhlee7125@gmail.com>
2023-02-20 11:49:16 +0900

    feat: updateDarkMode 함수를 이용하여 html 요소에 dark 클래스 추가 및 제거 토글 구현

--- Ellie/todolist/src/context/DarkModeContext.jsx
+++ Ellie/todolist/src/context/DarkModeContext.jsx
@@ -4,7 +4,19 @@ export const DarkModeContext = createContext();
 
 export function DarkModeProvider({ children }) {
   const [darkMode, setDarkMode] = useState(false);
-  const toggleDarkMode = () => setDarkMode((mode) => !mode);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    updateDarkMode(!darkMode);
+  };
 
   return <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>{children}</DarkModeContext.Provider>;
+}
+
+// darkmode 스타일 클래스를 html에 적용
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
 }
