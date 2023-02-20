03798c9a8b05112c71d9191900823fede0913010
jihyunglee <jhlee7125@gmail.com>
2023-02-20 16:55:47 +0900

    feat: staleTime을 설정하여 refresh 상태로 cache에 보관

--- Ellie/ch08_query/src/App.js
+++ Ellie/ch08_query/src/App.js
@@ -1,5 +1,6 @@
 import MainProducts from "./components/MainProducts";
 import "./App.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -8,6 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <MainProducts />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
--- Ellie/ch08_query/src/components/Products.jsx
+++ Ellie/ch08_query/src/components/Products.jsx
@@ -6,15 +6,23 @@ export default function Products() {
   const [checked, setChecked] = useState(false);
   // react-query를 사용하여 네트워크 통신을 구현하였을 때 치명적인 함정이 존재한다
   // 브라우저를 클릭할 때마다, 토글할 때마다, 등등 모든 변화에 대해 매번 네트워크 요청이 반복적으로 이루어지고 있음을 확인할 수 있다
-  //
+  // 그 이유는 통신을 통해 받아온 데이터를 바로 stale(오래된) 상태로 간주하기 때문이다
+  // 따라서 옵션으로 얼마의 시간동안 cache된 상태를 유지하려고 하는지 지정해주는 것이 필요하다
   const {
     isLoading,
     error,
     data: products,
-  } = useQuery(["products", checked], async () => {
-    console.log("fetching...");
-    return fetch(`data/${checked ? "sale_" : ""}products.json`).then((res) => res.json());
-  });
+  } = useQuery(
+    ["products", checked],
+    async () => {
+      console.log("fetching...");
+      return fetch(`data/${checked ? "sale_" : ""}products.json`).then((res) => res.json());
+    },
+    {
+      // 5분 동안 refresh 상태로 cache를 보관
+      staleTime: 1000 * 60 * 5,
+    }
+  );
   // const [loading, error, products] = useProduct({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
 
