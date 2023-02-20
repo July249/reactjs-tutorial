c258b2fb13622a42c469495f423cec29c147a388
jihyunglee <jhlee7125@gmail.com>
2023-02-20 11:44:28 +0900

    feat: DarkModeProvider App.jsx에 적용 및 Header에 DarkMode 토글 버튼 구현

--- Ellie/todolist/src/App.jsx
+++ Ellie/todolist/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
+
   return (
-    <>
+    <DarkModeProvider>
       <Header filters={filters} filter={filter} onFilterChange={setFilter} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
--- Ellie/todolist/src/components/Header/Header.jsx
+++ Ellie/todolist/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useDarkMode } from "../../hooks/useDarkMode";
+import { MdOutlineLightMode, MdDarkMode } from "react-icons/md";
 import styles from "./Header.module.css";
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.header}>
+      <button type="button" onClick={toggleDarkMode}>
+        {darkMode ? <MdOutlineLightMode /> : <MdDarkMode />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
