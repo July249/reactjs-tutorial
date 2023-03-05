de47941d142c6a1249b70b56329512ba5e99a739
jihyunglee <jhlee7125@gmail.com>
2023-03-05 20:50:00 +0900

    [Dave] props로 App에서 Header로 전달하지 않고 useContext를 이용하여 전달

--- Dave/21_context_api_and_usecontext/src/components/Header.js
+++ Dave/21_context_api_and_usecontext/src/components/Header.js
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
 
-const Header = ({ title, width }) => {
+const Header = ({ title }) => {
+  const { width } = useContext(DataContext);
+
   return (
     <header className="Header">
       <Link to="/" style={{ textDecoration: "none" }}>
