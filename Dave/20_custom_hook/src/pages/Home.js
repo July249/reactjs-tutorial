d8784b8ba633b6257ad2e1610a8e6d8f3bbb4772
jihyunglee <jhlee7125@gmail.com>
2023-03-05 18:23:17 +0900

    [Dave] fetchError, isLoading에 따른 Home 페이지 컴포넌트 jsx 변경

--- Dave/20_custom_hook/src/pages/Home.js
+++ Dave/20_custom_hook/src/pages/Home.js
@@ -1,13 +1,21 @@
-import Feed from '../components/Feed';
+import Feed from "../components/Feed";
 
-const Home = ({ posts }) => {
+const Home = ({ posts, fetchError, isLoading }) => {
   return (
     <main className="Home">
-      {posts.length ? (
-        <Feed posts={posts} />
-      ) : (
-        <p style={{ marginTop: '2rem' }}>No posts to display.</p>
+      {isLoading && <p className="statusMsg">Loading posts...</p>}
+      {fetchError && (
+        <p className="statusMsg" style={{ color: "red" }}>
+          {fetchError}
+        </p>
       )}
+      {!isLoading &&
+        !fetchError &&
+        (posts.length ? (
+          <Feed posts={posts} />
+        ) : (
+          <p className="statusMsg">No posts to display.</p>
+        ))}
     </main>
   );
 };
