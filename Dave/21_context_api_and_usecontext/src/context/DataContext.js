babbdc93f8fb3e5553232825550aacf0ffc142a6
jihyunglee <jhlee7125@gmail.com>
2023-03-05 20:37:16 +0900

    [Dave] DataContext 생성

---
+++ Dave/21_context_api_and_usecontext/src/context/DataContext.js
@@ -0,0 +1,7 @@
+import { createContext, useState, useEffect } from "react";
+
+const DataContext = createContext();
+
+export const DataProvider = ({ children }) => {
+  return <DataContext.Provider value={{}}>{children}</DataContext.Provider>;
+};
