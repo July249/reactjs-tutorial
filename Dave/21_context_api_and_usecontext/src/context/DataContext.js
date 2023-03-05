4ad704c242419c28139a9f534de3eef9e2ce395f
jihyunglee <jhlee7125@gmail.com>
2023-03-05 21:39:41 +0900

    [Dave] App 컴포넌트의 Router 구축을 위한 하위 컴포넌트에 값을 전달하기 위한 과정에서 DataProvider 설정 - App.js 구현 내용을 거의 그대로 차용

--- Dave/21_context_api_and_usecontext/src/context/DataContext.js
+++ Dave/21_context_api_and_usecontext/src/context/DataContext.js
@@ -1,7 +1,110 @@
 import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { format } from "date-fns";
+import api from "../api/posts";
+import useAxiosFetch from "../hooks/useAxiosFetch";
 
-const DataContext = createContext();
+export const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
-  return <DataContext.Provider value={{}}>{children}</DataContext.Provider>;
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
+
+  let navigate = useNavigate();
+
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3500/posts"
+  );
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data]);
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.title.toLowerCase().includes(search.toLowerCase()) ||
+        post.body.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+
+    // Create Operation
+    try {
+      const res = await api.post("/posts", newPost);
+      const allPosts = [...posts, res.data];
+      setPosts(allPosts);
+      setPostTitle("");
+      setPostBody("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
+  // Edit funtion
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    // Update Operation
+    try {
+      const res = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map((post) => (post.id === id ? { ...res.data } : post)));
+      setEditTitle("");
+      setEditBody("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
+  // Delete Operation
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter((post) => post.id !== id);
+      setPosts(postsList);
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
+  return (
+    <DataContext.Provider
+      value={{
+        search,
+        setSearch,
+        searchResults,
+        fetchError,
+        isLoading,
+        postTitle,
+        setPostTitle,
+        postBody,
+        setPostBody,
+        handleSubmit,
+        posts,
+        editTitle,
+        setEditTitle,
+        editBody,
+        setEditBody,
+        handleEdit,
+        handleDelete,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
 };
