fb08f2e7e8f308100b2b70846463ba7648f35ade
jihyunglee <jhlee7125@gmail.com>
2023-03-05 18:31:02 +0900

    [Dave] Tutorial 21 - initial setting

---
+++ Dave/21_context_api_and_usecontext/src/components/Header.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
+
+const Header = ({ title, width }) => {
+  return (
+    <header className="Header">
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <h1>{title}</h1>
+        {width < 768 ? (
+          <FaMobileAlt />
+        ) : width < 992 ? (
+          <FaTabletAlt />
+        ) : (
+          <FaLaptop />
+        )}
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
