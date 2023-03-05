372ff76d69e2eb63afbd9c83b2168db47eb53ae1
jihyunglee <jhlee7125@gmail.com>
2023-03-05 17:33:10 +0900

    [Dave] width 사이즈에 따른 svg icon 변경

--- Dave/20_custom_hook/src/components/Header.js
+++ Dave/20_custom_hook/src/components/Header.js
@@ -1,10 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
 
-const Header = ({ title }) => {
+const Header = ({ title, width }) => {
   return (
     <header className="Header">
-      <Link to="/" style={{ textDecoration: 'none' }}>
+      <Link to="/" style={{ textDecoration: "none" }}>
         <h1>{title}</h1>
+        {width < 768 ? (
+          <FaMobileAlt />
+        ) : width < 992 ? (
+          <FaTabletAlt />
+        ) : (
+          <FaLaptop />
+        )}
       </Link>
     </header>
   );
