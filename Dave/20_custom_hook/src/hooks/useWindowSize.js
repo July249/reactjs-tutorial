83022a33e9f7b4e3da45880fd9bd681b8019c137
jihyunglee <jhlee7125@gmail.com>
2023-03-05 17:50:50 +0900

    [Dave] useWindowSize의 useEffect clean-up 수정

--- Dave/20_custom_hook/src/hooks/useWindowSize.js
+++ Dave/20_custom_hook/src/hooks/useWindowSize.js
@@ -18,12 +18,7 @@ const useWindowSize = () => {
 
     window.addEventListener("resize", handleResize);
 
-    const cleanUp = () => {
-      console.log("runs if a useEffect dep changes");
-      window.removeEventListener("resize", handleResize);
-    };
-
-    return cleanUp;
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return windowSize;
