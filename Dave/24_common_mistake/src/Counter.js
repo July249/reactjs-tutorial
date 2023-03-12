0085c6568406cfafb406ab4757672411c62e8925
jihyunglee <jhlee7125@gmail.com>
2023-03-12 14:41:02 +0900

    feat: create handleAdd, handleSubtract function

--- Dave/24_common_mistake/src/Counter.js
+++ Dave/24_common_mistake/src/Counter.js
@@ -3,12 +3,20 @@ import { useState } from 'react';
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+
   return (
     <section>
       <h1>{count}</h1>
       <div className='row'>
-        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+        <button onClick={handleAdd}>+</button>
+        <button onClick={handleSubtract}>-</button>
       </div>
       <div className='row'>
         <button onClick={() => setCount(0)}>Reset</button>
