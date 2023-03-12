872f36768042047af2a33247268fc540b39c3baf
jihyunglee <jhlee7125@gmail.com>
2023-03-12 23:57:36 +0900

    Achieving referential equality for an array with useMemo

--- Dave/26_usememo/src/App.js
+++ Dave/26_usememo/src/App.js
@@ -1,14 +1,41 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo, useCallback } from 'react';
 
-const fib = (n) => {
-  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+// const fib = (n) => {
+// return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+// };
+
+const getArray = () => {
+  for (let i = 0; i < 10000000000; i++) {
+    // do someting expensive
+  }
+  return ['Dave', 'Gray'];
 };
 
 function App() {
   const [userNumber, setUserNumber] = useState('');
   const [randomInput, setRandomInput] = useState('');
 
-  const fibNumber = useMemo(() => fib(userNumber), [userNumber]);
+  // const myArray = getArray();
+  /**
+   * it makes too heavy!
+   * So, use useMemo!
+   */
+  const myArray = useMemo(() => getArray(), []);
+
+  const fib = useCallback((n) => {
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  }, []);
+
+  // const fib = (n) => {
+  //   return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  // };
+  /**
+   * As it should be now that it's inside the component,
+   * fib is new everytime when you type on Random Input.
+   * Now we have the lag issue, again
+   */
+
+  const fibNumber = useMemo(() => fib(userNumber), [userNumber, fib]);
   /**
    * if userNumber = 20, fibNumber = 6765
    * if userNumber = 33, fibNumber = 3524578
@@ -24,13 +51,14 @@ function App() {
    */
 
   useEffect(() => {
-    console.log('New number');
+    // console.log('New number');
     /**
      * Since fibNumber is primitive value, 'New number' doesn't print on console even if I type some value on RandomInput.
      * So, the dependency of useEffect works well!
      *
      */
-  }, [fibNumber]);
+    console.log('New array');
+  }, [myArray /* fibNumber */]);
 
   return (
     <main className='App'>
