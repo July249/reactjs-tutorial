52bd3e7c2684466a7d6a175f8c60cc88529fa777
jihyunglee <jhlee7125@gmail.com>
2023-03-12 12:31:10 +0900

    refactor: Refactoring Nav

--- Dave/22_easy_peasy_redux/src/components/Nav.js
+++ Dave/22_easy_peasy_redux/src/components/Nav.js
@@ -1,31 +1,45 @@
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { DataContext } from "../context/DataContext";
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useStoreState, useStoreActions } from 'easy-peasy';
 
 const Nav = () => {
-  const { search, setSearch } = useContext(DataContext);
+  const posts = useStoreState((state) => state.posts);
+  const search = useStoreState((state) => state.search);
+  const setSearch = useStoreState((actions) => actions.setSearch);
+  const setSearchResults = useStoreActions(
+    (actions) => actions.setSearchResults
+  );
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.title.toLowerCase().includes(search.toLowerCase()) ||
+        post.body.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search, setSearchResults]);
 
   return (
-    <nav className="Nav">
-      <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor="search">Search Posts</label>
+    <nav className='Nav'>
+      <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor='search'>Search Posts</label>
         <input
-          id="search"
-          type="text"
-          placeholder="Search Posts"
+          id='search'
+          type='text'
+          placeholder='Search Posts'
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
       </form>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to="/post">Post</Link>
+          <Link to='/post'>Post</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to='/about'>About</Link>
         </li>
       </ul>
     </nav>
