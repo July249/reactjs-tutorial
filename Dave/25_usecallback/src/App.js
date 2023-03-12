56af642c1a8d8321d189b03ec9eceb40771a1a7f
jihyunglee <jhlee7125@gmail.com>
2023-03-12 17:13:41 +0900

    An example without referential equality

--- Dave/25_usecallback/src/App.js
+++ Dave/25_usecallback/src/App.js
@@ -1,7 +1,35 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  // userInput exists only for re-rendering when you type.
+  const [userInput, setUserInput] = useState('');
+  const [result, setResult] = useState(0);
+  const [num1] = useState(4);
+  const [num2] = useState(5);
+
+  /**
+   * The 'sum' function makes the dependencies of useEffect Hook (at line 15) change on every render.
+   * Move it inside the useEffect callback.
+   * Alternatively, wrap the definition of 'sum' in its own useCallback() Hook.
+   */
+  const sum = () => num1 + num2;
+
+  useEffect(() => {
+    console.log(`New sum. Value: ${sum()}`);
+    setResult();
+  }, [sum /* dep. is object! */]);
+
+  return (
+    <main className='App'>
+      <input
+        type='text'
+        placeholder='input'
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <h1>Output: {userInput || '--'}</h1>
+    </main>
+  );
+};
 
 export default App;
