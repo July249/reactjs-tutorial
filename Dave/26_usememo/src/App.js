aa536fb6d3c662c0ced7ee0cc5ff1eb76a294f16
jihyunglee <jhlee7125@gmail.com>
2023-03-12 19:19:48 +0900

    initial setting

--- Dave/26_usememo/src/App.js
+++ Dave/26_usememo/src/App.js
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+
+const fib = (n) => {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+};
+
 function App() {
-  return <div className='App'></div>;
+  const [userNumber, setUserNumber] = useState('');
+  const [randomInput, setRandomInput] = useState('');
+
+  const fibNumber = fib(userNumber);
+
+  return (
+    <main className='App'>
+      <label>Fibonacci Sequence:</label>
+      <input
+        type='number'
+        value={userNumber}
+        placeholder='Position'
+        onChange={(e) => setUserNumber(e.target.value)}
+      />
+      <p>Number: {fibNumber || '--'}</p>
+      <br />
+      <br />
+      <label>Random Input:</label>
+      <input
+        type='text'
+        value={randomInput}
+        placeholder='Random Input'
+        onChange={(e) => setRandomInput(e.target.value)}
+      />
+      <p>{randomInput}</p>
+    </main>
+  );
 }
 
 export default App;
