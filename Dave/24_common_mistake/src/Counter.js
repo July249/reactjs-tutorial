e4c8a8928c26ae34a0bca539601974f9692ff105
jihyunglee <jhlee7125@gmail.com>
2023-03-12 14:38:22 +0900

    feat: setCount change

--- Dave/24_common_mistake/src/Counter.js
+++ Dave/24_common_mistake/src/Counter.js
@@ -7,8 +7,8 @@ const Counter = () => {
     <section>
       <h1>{count}</h1>
       <div className='row'>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
       </div>
       <div className='row'>
         <button onClick={() => setCount(0)}>Reset</button>
