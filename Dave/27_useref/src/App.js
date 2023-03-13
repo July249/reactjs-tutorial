8d5c16a019583f8f11565b498f1e6eb4742231c1
jihyunglee <jhlee7125@gmail.com>
2023-03-13 18:49:56 +0900

    initial setting

--- Dave/27_useref/src/App.js
+++ Dave/27_useref/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [randomInput, setRandomInput] = useState('');
+
+  const handleChage = (e) => {
+    setRandomInput(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <input
+        type='text'
+        value={randomInput}
+        placeholder='Random Input'
+        onChange={handleChage}
+      />
+      <br />
+      <br />
+      <p>{randomInput}</p>
+    </main>
   );
 }
 
