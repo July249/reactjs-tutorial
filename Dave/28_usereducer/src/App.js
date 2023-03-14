940c3344dd4ca01534b3dc0ed5289b04b191fd5a
jihyunglee <jhlee7125@gmail.com>
2023-03-14 16:41:06 +0900

    Recommended: Action object lookup and dot notation

--- Dave/28_usereducer/src/App.js
+++ Dave/28_usereducer/src/App.js
@@ -1,14 +1,21 @@
-import { useState, useReducer } from 'react';
+import { useReducer } from 'react';
+
+const ACTION = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+  NEW_USER_INPUT: 'newUserInput',
+  TG_COLOR: 'toggleColor',
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'increment':
+    case ACTION.INCREMENT:
       return { ...state, count: state.count + 1 };
-    case 'decrement':
+    case ACTION.DECREMENT:
       return { ...state, count: state.count - 1 };
-    case 'newUserInput':
+    case ACTION.NEW_USER_INPUT:
       return { ...state, userInput: action.payload };
-    case 'toggleColor':
+    case ACTION.TG_COLOR:
       return { ...state, color: !state.color };
     default:
       throw new Error('Not Expected Action!');
@@ -37,16 +44,18 @@ function App() {
         type='text'
         value={state.userInput}
         onChange={(e) =>
-          dispatch({ type: 'newUserInput', payload: e.target.value })
+          dispatch({ type: ACTION.NEW_USER_INPUT, payload: e.target.value })
         }
       />
       <br />
       <br />
       <p>{state.count}</p>
       <section>
-        <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
-        <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-        <button onClick={() => dispatch({ type: 'toggleColor' })}>Color</button>
+        <button onClick={() => dispatch({ type: ACTION.DECREMENT })}>-</button>
+        <button onClick={() => dispatch({ type: ACTION.INCREMENT })}>+</button>
+        <button onClick={() => dispatch({ type: ACTION.TG_COLOR })}>
+          Color
+        </button>
       </section>
       <br />
       <br />
