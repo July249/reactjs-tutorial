520d3aefbad7531ec7735897c9b61e7c8ba44e76
jihyunglee <jhlee7125@gmail.com>
2023-03-14 18:10:21 +0900

    useLayoutEffect vs. useEffect

--- Dave/30_uselayouteffect/src/App.js
+++ Dave/30_uselayouteffect/src/App.js
@@ -1,6 +1,30 @@
 import { useState, useRef, useEffect } from 'react';
 
 function App() {
+  /**
+   * KeyNote
+   *
+   * The only difference is exactly when useLayoutEffect vs. useEffect runs the code that's inside of the effect
+   * and as the name indicates useEffect or useLayoutEffect the code inside is really considered to be a side effect something that changes after something else has changed in the component
+   * and that's why we put a dependency inside the array.
+   *
+   * We're listening or looking for a change and if say number changes then we're going to run this code
+   * and that's the same for useEffect and useLayoutEffect
+   *
+   * The difference is useEffect is asynchronous
+   * so it will not delay painting the DOM to the browser
+   *
+   * but useLayoutEffect is synchronous and it will delay painting the DOM to the browser
+   * it wants to run its code before painting to the browser
+   *
+   * When does this come into play well really it makes useEffect the correct and more performance choice about 99% of the time
+   *
+   * However there's that 1% of the time when you want to choose useLayoutEffect over useEffect
+   *
+   * That's because you want the code to run before it directly modifies the DOM
+   * and that modification of course, is observable to the user
+   * You don't want the user to observe that change
+   */
   const [number, setNumber] = useState(0);
   const [sectionStyle, setSectionStyle] = useState({});
   const sectionRef = useRef();
