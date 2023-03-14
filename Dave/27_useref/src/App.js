da5103ab1ead9c589158d5c71e1205686b7a86e3
jihyunglee <jhlee7125@gmail.com>
2023-03-14 15:34:58 +0900

    useState vs useRef

--- Dave/27_useref/src/App.js
+++ Dave/27_useref/src/App.js
@@ -1,8 +1,16 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
   const [randomInput, setRandomInput] = useState('');
-
+  const renders = useRef(0);
+  /**
+   * Two Important Rules of useRef
+   *
+   * 1. Value persists that means it stays the same between renders
+   * 2. Updating the reference does not trigger a re-render
+   *
+   * These two things are big difference between useState and useRef!
+   */
   const handleChage = (e) => {
     setRandomInput(e.target.value);
   };
