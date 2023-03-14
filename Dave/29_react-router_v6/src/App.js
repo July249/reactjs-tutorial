1df9b01113f2f98fed4a10163329fe4c261e9ae0
jihyunglee <jhlee7125@gmail.com>
2023-03-14 17:31:40 +0900

    Nested Routes and additional Routes

--- Dave/29_react-router_v6/src/App.js
+++ Dave/29_react-router_v6/src/App.js
@@ -77,22 +77,24 @@ function App() {
         element={<Layout search={search} setSearch={setSearch} />}
       >
         <Route index element={<Home posts={searchResults} />} />
-        <Route
-          path='/post'
-          element={
-            <NewPost
-              handleSubmit={handleSubmit}
-              postTitle={postTitle}
-              setPostTitle={setPostTitle}
-              postBody={postBody}
-              setPostBody={setPostBody}
-            />
-          }
-        />
-        <Route
-          path='/post/:id'
-          element={<PostPage posts={posts} handleDelete={handleDelete} />}
-        />
+        <Route path='post'>
+          <Route
+            index
+            element={
+              <NewPost
+                handleSubmit={handleSubmit}
+                postTitle={postTitle}
+                setPostTitle={setPostTitle}
+                postBody={postBody}
+                setPostBody={setPostBody}
+              />
+            }
+          />
+          <Route
+            path='/post/:id'
+            element={<PostPage posts={posts} handleDelete={handleDelete} />}
+          />
+        </Route>
         <Route path='/about' element={<About />} />
         <Route path='*' element={<Missing />} />
       </Route>
