4211116aab59010f06f0881f4f8f1ca00683ac0e
jihyunglee <jhlee7125@gmail.com>
2023-03-14 18:54:54 +0900

    useImperativeHandle Hook 사용하기

--- Dave/31_useimperativehandle/src/App.js
+++ Dave/31_useimperativehandle/src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+/**
+ * useImperativeHandle를 이용하면, 자식 컴포넌트에서 정의한 customFocus 메서드를 부모 컴포넌트에서 사용할 수 있다
+ */
+
+import { useRef } from 'react';
+import MyInput from './MyInput';
 
 const App = () => {
-  return <div>App</div>;
+  const inputRef = useRef();
+
+  return (
+    <div style={{ margin: '20px 10px' }}>
+      <MyInput ref={inputRef} />
+      <button
+        style={{ marginLeft: '10px' }}
+        onClick={() => inputRef.current.customFocus()}
+      >
+        인풋 포커스
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- Dave/31_useimperativehandle/src/MyInput.js
+++ Dave/31_useimperativehandle/src/MyInput.js
@@ -1,7 +1,14 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 function MyInput(props, ref) {
-  return <input ref={ref} />;
+  const inputRef = useRef();
+  useImperativeHandle(ref, () => ({
+    customFocus: () => {
+      inputRef.current.focus();
+      inputRef.current.value = '포커스 되었습니다.';
+    },
+  }));
+  return <input ref={inputRef} />;
 }
 
 export default forwardRef(MyInput);
