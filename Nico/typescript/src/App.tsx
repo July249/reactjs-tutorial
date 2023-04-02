1287ac50b34aef04515dd4a646d68f4f60754058
jihyunglee <jhlee7125@gmail.com>
2023-04-03 04:35:13 +0900

    lec03-02 typing the props

--- Nico/typescript/src/App.tsx
+++ Nico/typescript/src/App.tsx
@@ -3,7 +3,8 @@ import Circle from './Circle';
 function App() {
   return (
     <div>
-      <Circle />
+      <Circle bgColor='teal' />
+      <Circle bgColor='tomato' />
     </div>
   );
 }
--- Nico/typescript/src/Circle.tsx
+++ Nico/typescript/src/Circle.tsx
@@ -1,9 +1,22 @@
 import styled from 'styled-components';
 
-const Container = styled.div``;
+interface ContainerProps {
+  bgColor: string;
+}
 
-function Circle() {
-  return <Container></Container>;
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 50%;
+`;
+
+interface CircleProps {
+  bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor} />;
 }
 
 export default Circle;
