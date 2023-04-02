b05b6d8141c38b5d986a7244017b12b635b3797e
jihyunglee <jhlee7125@gmail.com>
2023-04-03 02:28:22 +0900

    lec02-04 pseudo selectors

--- Nico/styled-components/src/App.js
+++ Nico/styled-components/src/App.js
@@ -19,14 +19,31 @@ const rotateAnimation = keyframes`
 const Box = styled.div`
   width: 200px;
   height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: tomato;
   animation: ${rotateAnimation} 2s linear infinite;
+
+  span {
+    font-size: 36px;
+
+    &:hover {
+      font-size: 48px;
+    }
+
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box>
+        <span>😃</span>
+      </Box>
     </Wrapper>
   );
 }
