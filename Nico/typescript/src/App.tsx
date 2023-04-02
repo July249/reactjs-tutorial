1949c3ee8ea3edb2a5c16a62279c0cee557a5a81
jihyunglee <jhlee7125@gmail.com>
2023-04-03 04:24:58 +0900

    lec03-02 init

--- Nico/typescript/src/App.tsx
+++ Nico/typescript/src/App.tsx
@@ -1,7 +1,11 @@
-import styled from 'styled-components';
+import Circle from './Circle';
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Circle />
+    </div>
+  );
 }
 
 export default App;
---
+++ Nico/typescript/src/Circle.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+function Circle() {
+  return <Container></Container>;
+}
+
+export default Circle;
