39e4e602887e9770e9e959b2d8fc0babefa97280
jihyunglee <jhlee7125@gmail.com>
2023-04-03 03:10:11 +0900

    lec02-07 Applying light or dark themes as simply

--- Nico/styled-components/src/App.js
+++ Nico/styled-components/src/App.js
@@ -1,52 +1,22 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  } 50% {
-    border-radius: 50%;
-  } 100% {
-    transform: rotate(360deg);
-    border-radius: 0;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-  &:active {
-    opacity: 0;
-  }
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  display: flex;
+  width: 100vw;
+  height: 100vh;
   justify-content: center;
   align-items: center;
-  background-color: tomato;
-  animation: ${rotateAnimation} 2s linear infinite;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
 
-  // Targeting the Emoji component whatever this element is
-  // So no longer need to be span element
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as='p'>😃</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
--- Nico/styled-components/src/index.js
+++ Nico/styled-components/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+
+const darkTheme = {
+  textColor: 'white',
+  backgroundColor: 'black',
+};
+
+const lightTheme = {
+  textColor: 'black',
+  backgroundColor: 'white',
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
