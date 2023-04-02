6a411e371c171e1989d7b168103a71ad1aae7f35
jihyunglee <jhlee7125@gmail.com>
2023-04-03 02:23:54 +0900

    lec02-04 animations

--- Nico/styled-components/src/App.js
+++ Nico/styled-components/src/App.js
@@ -1,59 +1,33 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const TopDiv = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  } 50% {
+    border-radius: 50%;
+  } 100% {
+    transform: rotate(360deg);
+    border-radius: 0;
+  }
+`;
+
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50%;
-`;
-
-const Btn = styled.button`
-  color: white;
+  width: 200px;
+  height: 200px;
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
-
-// Adding required attribute of input for each Input, you can use styled-components attrs like this!
-// const Input = styled.input`
-//   background-color: tomato;
-// `;
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
+  animation: ${rotateAnimation} 2s linear infinite;
 `;
 
 function App() {
   return (
-    <TopDiv as='header'>
-      {/* <Box bgColor='teal' />
-      <Circle bgColor='tomato' /> */}
-      {/* <Btn>Log in</Btn>
-      <Btn as='a' href='/'>
-        Log in
-      </Btn> */}
-      {/* Adding required attribute of input for each Input, you can use styled-components attrs! */}
-      {/* <Input required />
-      <Input required />
-      <Input required />
-      <Input required />
-      <Input required />
-      <Input required />
-      <Input required />
-      <Input required /> */}
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </TopDiv>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 
