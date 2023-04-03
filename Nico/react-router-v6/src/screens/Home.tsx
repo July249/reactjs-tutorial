b76bc9004935f6aacd93735da4eb8841812af3fe
jihyunglee <jhlee7125@gmail.com>
2023-04-04 02:06:37 +0900

    lec04-08 useSearchParms

--- Nico/react-router-v6/src/screens/Home.tsx
+++ Nico/react-router-v6/src/screens/Home.tsx
@@ -1,7 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import { users } from '../db';
 
 function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams({});
+  console.log(readSearchParams);
+  console.log(readSearchParams.has('day'));
+  console.log(readSearchParams.get('day'));
+  console.log(readSearchParams.getAll('day'));
+  console.log(readSearchParams.toString());
+  console.log(readSearchParams.keys());
+  console.log(readSearchParams.values());
+  console.log(readSearchParams.entries());
+  console.log(readSearchParams.sort);
+
+  // setSearchParams is very useful for navigating to a new route with new search params
+  // especially when you want to update the search params without reloading the page
+  // like redirect user splash screen to home page after 30 seconds
+
+  // setTimeout(() => {
+  //   setSearchParams({ day: 'today', tomorrow: '124' });
+  // }, 30000);
+
   return (
     <div>
       <ul>
