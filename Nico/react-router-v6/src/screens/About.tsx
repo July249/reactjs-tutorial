848e33825f9cd657cff178694b23db9e43483106
jihyunglee <jhlee7125@gmail.com>
2023-04-04 01:52:54 +0900

    lec04-07 same example of User and Followers case

--- Nico/react-router-v6/src/screens/About.tsx
+++ Nico/react-router-v6/src/screens/About.tsx
@@ -1,4 +1,8 @@
+import { useOutletContext } from 'react-router-dom';
+
 function About() {
+  // share the context from the parent route element, in this case Root
+  console.log(useOutletContext());
   return <h1>About</h1>;
 }
 
