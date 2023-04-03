8cbfe171ec143fc54a0584e43808aaed022a5f07
jihyunglee <jhlee7125@gmail.com>
2023-04-04 01:40:10 +0900

    lec04-06 connect Follower page the children of User page

--- Nico/react-router-v6/src/screens/users/User.tsx
+++ Nico/react-router-v6/src/screens/users/User.tsx
@@ -1,11 +1,18 @@
 import { useParams } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { users } from '../../db';
 
 function User() {
   const { id } = useParams();
   return (
     <div>
-      User with it {id} is named: {users[Number(id) - 1].name}
+      <h1>
+        User with it {id} is named: {users[Number(id) - 1].name}
+      </h1>
+      <hr />
+      {/* By using 'followers' not '/followers', this link has relatively directory root */}
+      <Link to='followers'>see followers</Link>
+      <Outlet />
     </div>
   );
 }
