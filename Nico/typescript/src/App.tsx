7a9775cea4f55b1fc84f369d50a0a0fcbc0371ed
jihyunglee <jhlee7125@gmail.com>
2023-04-03 22:43:09 +0900

    lec03-06 style props control to make it based on given theme

--- Nico/typescript/src/App.tsx
+++ Nico/typescript/src/App.tsx
@@ -1,31 +1,20 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 // import Circle from './Circle';
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 function App() {
-  const [value, setValue] = useState('');
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // console.log(event.currentTarget.value);
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('hello', value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type='text'
-          placeholder='username'
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <Title>Hello World</Title>
+    </Container>
   );
 }
 
