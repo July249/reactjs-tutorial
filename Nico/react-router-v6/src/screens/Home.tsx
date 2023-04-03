588f86f330d9a39c9436dded30602f3e5b64d77a
jihyunglee <jhlee7125@gmail.com>
2023-04-04 01:25:59 +0900

    lec04-05 setup user page link

--- Nico/react-router-v6/src/screens/Home.tsx
+++ Nico/react-router-v6/src/screens/Home.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import { users } from '../db';
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
