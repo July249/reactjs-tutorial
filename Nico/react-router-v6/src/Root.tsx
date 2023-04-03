221b25c3ca7a628178d3483377f8fd974bc5c134
jihyunglee <jhlee7125@gmail.com>
2023-04-04 00:31:19 +0900

    lec04-03 adding Header component to navigate url link

--- Nico/react-router-v6/src/Root.tsx
+++ Nico/react-router-v6/src/Root.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function Root() {
   return (
     <div>
+      <Header />
       <Outlet />
     </div>
   );
