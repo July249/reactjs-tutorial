7ebad59633d4f18b2e9d2a38cfbed230a3c9964d
jihyunglee <jhlee7125@gmail.com>
2023-04-03 22:27:52 +0900

    lec03-05 the way to handling form event by using typescript

--- Nico/typescript/src/App.tsx
+++ Nico/typescript/src/App.tsx
@@ -1,10 +1,30 @@
-import Circle from './Circle';
+import { useState } from 'react';
+// import Circle from './Circle';
 
 function App() {
+  const [value, setValue] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('hello', value);
+  };
   return (
     <div>
-      <Circle bgColor='teal' borderColor='black' />
-      <Circle bgColor='tomato' />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type='text'
+          placeholder='username'
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
