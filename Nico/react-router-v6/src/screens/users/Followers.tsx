7997c4048440f0a5006c4d022a3c15672630b27e
jihyunglee <jhlee7125@gmail.com>
2023-04-04 01:52:04 +0900

    lec04-07 get Outlet context data by using useOutletContext() in the chidren Components

--- Nico/react-router-v6/src/screens/users/Followers.tsx
+++ Nico/react-router-v6/src/screens/users/Followers.tsx
@@ -1,5 +1,13 @@
+import { useOutletContext } from 'react-router-dom';
+
+interface FollowersContext {
+  nameOfUser: string;
+}
+
 function Followers() {
-  return <h1>Followers</h1>;
+  const { nameOfUser } = useOutletContext<FollowersContext>();
+
+  return <h1>Here are {nameOfUser}'s followers</h1>;
 }
 
 export default Followers;
