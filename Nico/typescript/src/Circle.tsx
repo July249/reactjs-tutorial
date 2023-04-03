e1feaa28fd9a74cc865f5d907371846c497da7aa
jihyunglee <jhlee7125@gmail.com>
2023-04-03 22:14:17 +0900

    lec03-04 automatically setup type of useState without any constraint

--- Nico/typescript/src/Circle.tsx
+++ Nico/typescript/src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 interface ContainerProps {
@@ -19,6 +20,14 @@ interface CircleProps {
 }
 
 function Circle({ bgColor, borderColor }: CircleProps) {
+  // automatically type inferred count is gonna be a number!
+  // const [count, setCount] = useState(1);
+  // so we can't pass a string to
+  // setCount('hello');
+
+  // if we want to pass a string or number to setCount, we can use generics
+  const [count, setCount] = useState<number | string>(1);
+  // but! usually we don't want to do this because we want to keep consistently using the same type
   return (
     <Container
       bgColor={bgColor}
