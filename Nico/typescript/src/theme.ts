2d5393b2a7c2d1fc6d597074f47c60324349530c
jihyunglee <jhlee7125@gmail.com>
2023-04-03 22:35:50 +0900

    lec03-06 setup DefaultTheme and light / dark theme

---
+++ Nico/typescript/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  textColor: 'black',
+  bgColor: 'white',
+  btnColor: 'tomato',
+};
+
+export const darkTheme: DefaultTheme = {
+  textColor: 'white',
+  bgColor: 'black',
+  btnColor: 'teal',
+};
