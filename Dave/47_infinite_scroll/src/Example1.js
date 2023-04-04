f9856347d27c3c7189d46b8c0a1c2ba863695a50
jihyunglee <jhlee7125@gmail.com>
2023-04-05 00:38:00 +0900

    setting: initial setting

---
+++ Dave/47_infinite_scroll/src/Example1.js
@@ -0,0 +1,14 @@
+const Example1 = () => {
+  return (
+    <>
+      <h1 id='top'>
+        &infin; Infinite Query &amp; Scroll
+        <br />
+        &infin; Ex. 1 - React only
+      </h1>
+      <p className='center'>
+        <a href='#top'>Back to Top</a>
+      </p>
+    </>
+  );
+};
