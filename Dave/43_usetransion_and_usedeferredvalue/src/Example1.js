de1a374d37585ec79170097282a52de22abb6344
jihyunglee <jhlee7125@gmail.com>
2023-04-05 17:48:37 +0900

    Applying useTransition

--- Dave/43_usetransion_and_usedeferredvalue/src/Example1.js
+++ Dave/43_usetransion_and_usedeferredvalue/src/Example1.js
@@ -1,20 +1,27 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 
 const Example1 = () => {
   const [count, setCount] = useState(0);
   const [item, setItem] = useState([]);
+  const [isPending, startTransition] = useTransition({ timeoutMs: 3000 });
 
   const handleClick = () => {
+    // urgent update
     setCount(count + 1);
-    const myArr = Array(20000)
-      .fill(1)
-      .map((el, i) => count + 20000 - i);
-    setItem(myArr);
+
+    // transition update (not urgent update)
+    startTransition(() => {
+      const myArr = Array(20000)
+        .fill(1)
+        .map((el, i) => count + 20000 - i);
+      setItem(myArr);
+    });
   };
 
   const content = (
     <div className='App'>
       <button onClick={handleClick}>{count}</button>
+      {isPending ? <p>Loading ...</p> : null}
       <ul>
         {item.map((item) => (
           <li key={item}>{item}</li>
