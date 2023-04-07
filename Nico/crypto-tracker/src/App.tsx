9ff8cc8e9340ee91647dd6a5cc7ec8abf7b04242
jihyunglee <jhlee7125@gmail.com>
2023-04-07 23:31:10 +0900

    design: set GlobalStyle

--- Nico/crypto-tracker/src/App.tsx
+++ Nico/crypto-tracker/src/App.tsx
@@ -1,7 +1,31 @@
 import Router from './Router';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
