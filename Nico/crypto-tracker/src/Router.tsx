c1658ccf585079bd87da9d80e20938f7802b21a2
jihyunglee <jhlee7125@gmail.com>
2023-04-08 06:30:35 +0900

    fix: Routes general setting

--- Nico/crypto-tracker/src/Router.tsx
+++ Nico/crypto-tracker/src/Router.tsx
@@ -7,7 +7,7 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Coins />} />
-        <Route path='/:coinId' element={<Coin />} />
+        <Route path='/:coinId/*' element={<Coin />} />
       </Routes>
     </BrowserRouter>
   );
