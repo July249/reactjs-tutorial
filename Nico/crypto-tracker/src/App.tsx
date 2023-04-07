9e4ad296102bbc5e50c1f56bd8752f66386db5f1
jihyunglee <jhlee7125@gmail.com>
2023-04-08 02:44:17 +0900

    design: @import is not recommanded method. So, I changed it link element type by using react-helmet-async

--- Nico/crypto-tracker/src/App.tsx
+++ Nico/crypto-tracker/src/App.tsx
@@ -1,9 +1,9 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Router from './Router';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
   ${reset}
 
   * {
@@ -23,6 +23,16 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   return (
     <>
+      <HelmetProvider>
+        <Helmet>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap'
+            rel='stylesheet'
+          />
+        </Helmet>
+      </HelmetProvider>
       <GlobalStyle />
       <Router />
     </>
