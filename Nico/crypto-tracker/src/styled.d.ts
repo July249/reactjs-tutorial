8ae55cca5fb25e14095a6e6fbb447d500d5e22a5
jihyunglee <jhlee7125@gmail.com>
2023-04-07 23:31:34 +0900

    design: styling

--- Nico/crypto-tracker/src/styled.d.ts
+++ Nico/crypto-tracker/src/styled.d.ts
@@ -2,8 +2,8 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    textColor: string;
     bgColor: string;
-    btnColor: string;
+    textColor: string;
+    accentColor: string;
   }
 }
--- Nico/crypto-tracker/src/theme.ts
+++ Nico/crypto-tracker/src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  textColor: 'black',
-  bgColor: 'white',
-  btnColor: 'tomato',
+  bgColor: '#2f3640',
+  textColor: '#f5f6fa',
+  accentColor: '#44bd32',
 };
