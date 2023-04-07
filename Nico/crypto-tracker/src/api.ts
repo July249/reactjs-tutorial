eb9f8f3cf05f6adb22b9a49176f86443c820e2f2
jihyunglee <jhlee7125@gmail.com>
2023-04-08 07:14:05 +0900

    feat: fetchCoinInfo와 fetchCoinTickers Promise 함수 생성

--- Nico/crypto-tracker/src/api.ts
+++ Nico/crypto-tracker/src/api.ts
@@ -1,11 +1,15 @@
 import { ICoin } from './routes/Coins';
-// export async function fetchCoins() {
-//   const response = await fetch('https://api.coinpaprika.com/v1/coins');
-//   const json = await response.json();
-//   return json;
-// }
 
-// To make above code, more simplify.
+const BASE_URL = 'https://api.coinpaprika.com/v3';
+
 export function fetchCoins(): Promise<ICoin[]> {
-  return fetch('https://api.coinpaprika.com/v3/coins').then((response) => response.json());
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
+}
+
+export function fetchCoinInfo(id: string): Promise<any> {
+  return fetch(`${BASE_URL}/coins/${id}`).then((response) => response.json());
+}
+
+export function fetchCoinTickers(id: string): Promise<any> {
+  return fetch(`${BASE_URL}/tickers/${id}`).then((response) => response.json());
 }
