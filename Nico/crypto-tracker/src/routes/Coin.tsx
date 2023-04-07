2409344a073734209755e0cfcfc1a560307bf071
jihyunglee <jhlee7125@gmail.com>
2023-04-08 06:30:52 +0900

    feat: nested routes - 2

--- Nico/crypto-tracker/src/routes/Coin.tsx
+++ Nico/crypto-tracker/src/routes/Coin.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Routes, Route, useLocation, useParams } from 'react-router';
+import { Link, Routes, Route, useLocation, useParams, useMatch } from 'react-router-dom';
 import Chart from './Chart';
 import Price from './Price';
 import styled from 'styled-components';
@@ -47,6 +47,27 @@ const OverviewItem = styled.div`
 `;
 const Description = styled.p`
   margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) => (props.isActive ? props.theme.accentColor : props.theme.textColor)};
+  a {
+    display: block;
+  }
 `;
 
 interface InfoData {
@@ -110,6 +131,8 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const priceMatch = useMatch('/:coinId/price');
+  const chartMatch = useMatch('/:coinId/chart');
 
   useEffect(() => {
     (async () => {
@@ -161,10 +184,18 @@ function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
           {/* Nested Routes */}
           <Routes>
-            <Route path={`/${coinId}/price`} element={<Price />} />
-            <Route path={`/${coinId}/chart`} element={<Chart />} />
+            <Route path='/:coinId/price' element={<Price />} />
+            <Route path='/:coinId/chart' element={<Chart />} />
           </Routes>
         </>
       )}
