787984e2600eb4a388425bba5276bab96ecca937
jihyunglee <jhlee7125@gmail.com>
2023-04-08 06:39:37 +0900

    feat: basic setting of react-query

--- Nico/crypto-tracker/src/index.tsx
+++ Nico/crypto-tracker/src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
+
+// react-query
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
