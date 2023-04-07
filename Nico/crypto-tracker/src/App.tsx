cb45331c7ea1e73ef05b77346f593a870c310e18
jihyunglee <jhlee7125@gmail.com>
2023-04-07 23:50:14 +0900

    design: Home Part One

--- Nico/crypto-tracker/src/App.tsx
+++ Nico/crypto-tracker/src/App.tsx
@@ -16,6 +16,7 @@ const GlobalStyle = createGlobalStyle`
   }
   a {
     text-decoration: none;
+    color: inherit;
   }
 `;
 
--- Nico/crypto-tracker/src/routes/Coins.tsx
+++ Nico/crypto-tracker/src/routes/Coins.tsx
@@ -1,11 +1,89 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: #fff;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+
+  a {
+    padding: 20px;
+    display: block;
+    transition: color 0.5s ease-in;
+  }
+
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
+// tempate data
+const coins = [
+  {
+    id: 'btc-bitcoin',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'eth-ethereum',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: 'coin',
+  },
+  {
+    id: 'hex-hex',
+    name: 'HEX',
+    symbol: 'HEX',
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: 'token',
+  },
+];
+
 function Coins() {
-  return <Title>Coins</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 }
 
 export default Coins;
