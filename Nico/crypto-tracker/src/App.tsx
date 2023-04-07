f7173bcc27d5d4d37bc0ac08530044fb741a21da
jihyunglee <jhlee7125@gmail.com>
2023-04-08 03:42:50 +0900

    design: Coin styling and create Chart and Price component

--- Nico/crypto-tracker/src/App.tsx
+++ Nico/crypto-tracker/src/App.tsx
@@ -10,9 +10,11 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
   body {
+    font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
     background-color: ${(props) => props.theme.bgColor};
-    color: ${(props) => props.theme.textColor};
+    color:${(props) => props.theme.textColor};
+    line-height: 1.2;
   }
   a {
     text-decoration: none;
--- Nico/crypto-tracker/src/routes/Coin.tsx
+++ Nico/crypto-tracker/src/routes/Coin.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { Routes, Route, useLocation, useParams } from 'react-router';
+import Chart from './Chart';
+import Price from './Price';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -23,6 +25,28 @@ const Title = styled.h1`
 const Loader = styled.span`
   display: block;
   text-align: center;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
 `;
 
 interface InfoData {
@@ -101,14 +125,48 @@ function Coin() {
       setPriceInfo(priceData);
       setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
         <Title>{state?.name || 'Loading'}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : <></>}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? 'Yes' : 'No'}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Routes>
+            <Route path={`/${coinId}/price`} element={<Price />} />
+            <Route path={`/${coinId}/chart`} element={<Chart />} />
+          </Routes>
+        </>
+      )}
     </Container>
   );
 }
