238887df9d4286c222c2348fd7f89256b131c88f
jihyunglee <jhlee7125@gmail.com>
2023-04-08 21:57:43 +0900

    feat: fetchCoinHistory API 생성

--- Nico/crypto-tracker/src/api.ts
+++ Nico/crypto-tracker/src/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = 'https://api.coinpaprika.com/v1';
+const CUSTOM_URL = 'https://ohlcv-api.nomadcoders.workers.dev?coinId';
 
 export function fetchCoins(): Promise<any> {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
@@ -10,4 +11,8 @@ export function fetchCoinInfo(id: string): Promise<any> {
 
 export function fetchCoinTickers(id: string): Promise<any> {
   return fetch(`${BASE_URL}/tickers/${id}`).then((response) => response.json());
+}
+
+export function fetchCoinHistory(id: string): Promise<any> {
+  return fetch(`${CUSTOM_URL}=${id}`).then((response) => response.json());
 }
