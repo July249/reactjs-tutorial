4096e1658ab58617d074d0849896c05466fcd72c
jihyunglee <jhlee7125@gmail.com>
2023-04-08 21:58:09 +0900

    feat: useQuery 사용을 통한 데이터 받아오기

--- Nico/crypto-tracker/src/routes/Chart.tsx
+++ Nico/crypto-tracker/src/routes/Chart.tsx
@@ -1,4 +1,14 @@
-function Chart() {
+import { useQuery } from 'react-query';
+import { fetchCoinHistory } from '../api';
+
+interface chartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: chartProps) {
+  const { isLoading, data } = useQuery(['ohlcv', coinId], () => fetchCoinHistory(coinId));
+  console.log(data);
+
   return <h1>Chart</h1>;
 }
 
