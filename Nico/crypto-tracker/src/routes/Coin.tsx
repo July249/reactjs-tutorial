b868228c9ce05144c23c1bdb029c7d8c7187203d
jihyunglee <jhlee7125@gmail.com>
2023-04-08 16:59:47 +0900

    feat: using useQuery

--- Nico/crypto-tracker/src/routes/Coin.tsx
+++ Nico/crypto-tracker/src/routes/Coin.tsx
@@ -1,32 +1,29 @@
-import { useEffect, useState } from 'react';
 import { Link, Routes, Route, useLocation, useParams, useMatch } from 'react-router-dom';
 import Chart from './Chart';
 import Price from './Price';
 import styled from 'styled-components';
-
-const Container = styled.div`
-  padding: 0 20px;
-  max-width: 480px;
-  margin: 0 auto;
-`;
-
-const Header = styled.header`
-  height: 10vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { useQuery } from 'react-query';
+import { fetchCoinInfo, fetchCoinTickers } from '../api';
 
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
-
 const Loader = styled.span`
-  display: block;
   text-align: center;
+  display: block;
 `;
-
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const Overview = styled.div`
   display: flex;
   justify-content: space-between;
@@ -38,6 +35,8 @@ const OverviewItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 33%;
+
   span:first-child {
     font-size: 10px;
     font-weight: 400;
@@ -48,24 +47,22 @@ const OverviewItem = styled.div`
 const Description = styled.p`
   margin: 20px 0px;
 `;
-
 const Tabs = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   margin: 25px 0px;
   gap: 10px;
 `;
-
 const Tab = styled.span<{ isActive: boolean }>`
   text-align: center;
   text-transform: uppercase;
   font-size: 12px;
   font-weight: 400;
   background-color: rgba(0, 0, 0, 0.5);
-  padding: 7px 0px;
   border-radius: 10px;
   color: ${(props) => (props.isActive ? props.theme.accentColor : props.theme.textColor)};
   a {
+    padding: 7px 0px;
     display: block;
   }
 `;
@@ -128,32 +125,25 @@ interface PriceData {
 function Coin() {
   const { coinId } = useParams();
   const { state } = useLocation();
-  const [loading, setLoading] = useState(true);
-  const [info, setInfo] = useState<InfoData>();
-  const [priceInfo, setPriceInfo] = useState<PriceData>();
   const priceMatch = useMatch('/:coinId/price');
   const chartMatch = useMatch('/:coinId/chart');
 
-  useEffect(() => {
-    (async () => {
-      // const response = await fetch('https://api.coingecko.com/api/v3/coins/' + coinId);
-      // const json = await response.json();
-      // short version of the above:
-      const infoData = await (await fetch('https://api.coinpaprika.com/v1/coins/' + coinId)).json();
-      const priceData = await (
-        await fetch('https://api.coinpaprika.com/v1/tickers/' + coinId)
-      ).json();
-
-      setInfo(infoData);
-      setPriceInfo(priceData);
-      setLoading(false);
-    })();
-  }, [coinId]);
+  // The key of react-query should be unique!
+  // const {} = useQuery(coinId, () => fetchCoinInfo(coinId));
+  // const {} = useQuery(coinId, () => fetchCoinTickers(coinId));
+  const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(['info', coinId], () =>
+    fetchCoinInfo(String(coinId))
+  );
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
+    ['tickers', coinId],
+    () => fetchCoinTickers(String(coinId))
+  );
+  const loading = infoLoading || tickersLoading;
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name ? state.name : loading ? 'Loadin...' : info?.name}</Title>
+        <Title>{state?.name ? state.name : loading ? 'Loadin...' : infoData?.name}</Title>
       </Header>
       {loading ? (
         <Loader>Loading...</Loader>
@@ -162,26 +152,26 @@ function Coin() {
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{info?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${info?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Open Source:</span>
-              <span>{info?.open_source ? 'Yes' : 'No'}</span>
+              <span>{infoData?.open_source ? 'Yes' : 'No'}</span>
             </OverviewItem>
           </Overview>
-          <Description>{info?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Suply:</span>
-              <span>{priceInfo?.total_supply}</span>
+              <span>{tickersData?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
+              <span>{tickersData?.max_supply}</span>
             </OverviewItem>
           </Overview>
           <Tabs>
