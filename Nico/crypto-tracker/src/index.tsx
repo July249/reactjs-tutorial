32576bf3915dad66f96ab605fcb97270df2ce0a7
jihyunglee <jhlee7125@gmail.com>
2023-04-09 16:46:37 +0900

    feat: recoil setup

--- Nico/crypto-tracker/src/index.tsx
+++ Nico/crypto-tracker/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 // react-query
 const queryClient = new QueryClient();
@@ -9,8 +10,10 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
