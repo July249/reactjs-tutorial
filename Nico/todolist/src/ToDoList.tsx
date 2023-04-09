57df55621ecf9a799ac301569de183afba324a62
jihyunglee <jhlee7125@gmail.com>
2023-04-09 18:51:51 +0900

    feat: ToDoList 기본 structure

--- Nico/todolist/src/ToDoList.tsx
+++ Nico/todolist/src/ToDoList.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
 function ToDoList() {
-  return null;
+  const [todo, setTodo] = useState<string>('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setTodo(value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(todo);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type='text' value={todo} onChange={onChange} placeholder='Write a to do' />
+        <button type='submit'>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
