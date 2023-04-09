6a3b7fa93312f92890fc12fa5422b0515044fe0a
jihyunglee <jhlee7125@gmail.com>
2023-04-09 19:12:55 +0900

    feat: check Error

--- Nico/todolist/src/ToDoList.tsx
+++ Nico/todolist/src/ToDoList.tsx
@@ -2,15 +2,20 @@ import { useState } from 'react';
 
 function ToDoList() {
   const [todo, setTodo] = useState<string>('');
+  const [todoError, setTodoError] = useState<string>('');
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = e;
+    setTodoError('');
     setTodo(value);
   };
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(todo);
+    if (todo.length < 10) {
+      return setTodoError('To do should be longer than 10 characters');
+    }
+    console.log('submit');
   };
 
   return (
@@ -18,6 +23,7 @@ function ToDoList() {
       <form onSubmit={onSubmit}>
         <input type='text' value={todo} onChange={onChange} placeholder='Write a to do' />
         <button type='submit'>Add</button>
+        {todoError && <p>{todoError}</p>}
       </form>
     </div>
   );
