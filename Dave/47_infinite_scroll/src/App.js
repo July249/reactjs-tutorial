154c777c70f589ed7a4a748ddbc03e61d64a8391
jihyunglee <jhlee7125@gmail.com>
2023-04-12 09:46:29 +0900

    feat: import Example2

--- Dave/47_infinite_scroll/src/App.js
+++ Dave/47_infinite_scroll/src/App.js
@@ -1,7 +1,8 @@
-import Example1 from './Example1';
+// import Example1 from './Example1';
+import Example2 from './Example2';
 
 function App() {
-  return <Example1 />;
+  return <Example2 />;
 }
 
 export default App;
