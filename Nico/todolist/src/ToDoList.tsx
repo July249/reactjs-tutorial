78295e3c930abb29c4f7f73f1081309fb9316c66
jihyunglee <jhlee7125@gmail.com>
2023-04-20 22:26:52 +0900

    feat: useForm 사용

--- Nico/todolist/src/ToDoList.tsx
+++ Nico/todolist/src/ToDoList.tsx
@@ -1,29 +1,42 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function ToDoList() {
-  const [todo, setTodo] = useState<string>('');
-  const [todoError, setTodoError] = useState<string>('');
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setTodoError('');
-    setTodo(value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (todo.length < 10) {
-      return setTodoError('To do should be longer than 10 characters');
-    }
-    console.log('submit');
-  };
+  // Before using react-hook-form
+  // const [todo, setTodo] = useState<string>('');
+  // const [todoError, setTodoError] = useState<string>('');
+  // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const {
+  //     currentTarget: { value },
+  //   } = e;
+  //   setTodoError('');
+  //   setTodo(value);
+  // };
+  // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   if (todo.length < 10) {
+  //     return setTodoError('To do should be longer than 10 characters');
+  //   }
+  //   console.log('submit');
+  // };
+  // return (
+  //   <div>
+  //     <form onSubmit={onSubmit}>
+  //       <input type='text' value={todo} onChange={onChange} placeholder='Write a to do' />
+  //       <button type='submit'>Add</button>
+  //       {todoError && <p>{todoError}</p>}
+  //     </form>
+  //   </div>
+  // );
+
+  // After using react-hook-form
+  const { register, watch } = useForm();
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input type='text' value={todo} onChange={onChange} placeholder='Write a to do' />
+      <form>
+        <input type='text' {...register('toDo')} placeholder='Write a to do' />
         <button type='submit'>Add</button>
-        {todoError && <p>{todoError}</p>}
       </form>
     </div>
   );
