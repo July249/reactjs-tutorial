1c0c49cd15696883d9fdb1c533d46868ee43ff14
jihyunglee <jhlee7125@gmail.com>
2023-04-21 00:13:02 +0900

    feat: react-hook-form useForm을 활용하여 수정

--- Nico/todolist/src/ToDoList.tsx
+++ Nico/todolist/src/ToDoList.tsx
@@ -1,124 +1,26 @@
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 interface IFormData {
   [key: string]: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  passwordCheck: string;
-  extraError: string;
+  todo: string;
 }
 
 function ToDoList() {
-  // Before using react-hook-form
-  // const [todo, setTodo] = useState<string>('');
-  // const [todoError, setTodoError] = useState<string>('');
-  // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const {
-  //     currentTarget: { value },
-  //   } = e;
-  //   setTodoError('');
-  //   setTodo(value);
-  // };
-  // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   if (todo.length < 10) {
-  //     return setTodoError('To do should be longer than 10 characters');
-  //   }
-  //   console.log('submit');
-  // };
-  // return (
-  //   <div>
-  //     <form onSubmit={onSubmit}>
-  //       <input type='text' value={todo} onChange={onChange} placeholder='Write a to do' />
-  //       <button type='submit'>Add</button>
-  //       {todoError && <p>{todoError}</p>}
-  //     </form>
-  //   </div>
-  // );
+  const { register, handleSubmit, setValue, setError } = useForm<IFormData>();
 
-  // After using react-hook-form
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IFormData>({
-    defaultValues: {
-      email: '@naver.com',
-    },
-  });
-  const onValid = (data: IFormData) => {
-    if (data.password !== data.passwordCheck) {
-      setError(
-        'passwordCheck',
-        {
-          message: 'Password does not match',
-        },
-        { shouldFocus: true } // focus on the input
-      );
-    }
-    setError('extraError', {
-      message: 'Server Offline',
-    });
+  const onAddTodo = (data: IFormData) => {
+    console.log('add to do', data.todo);
+    setValue('todo', ''); // reset input
   };
-  console.log(errors);
-
   return (
     <div>
-      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(onValid)}>
+      <form onSubmit={handleSubmit(onAddTodo)}>
         <input
           type='text'
-          {...register('email', {
-            required: 'Email is required',
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: 'Only naver.com email is allowed',
-            },
-          })}
-          placeholder='Email'
+          {...register('todo', { required: 'Please, write a to do' })}
+          placeholder='Write a to do'
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          type='text'
-          {...register('firstName', {
-            required: true,
-            validate: {
-              noNico: (value) => (value.includes('nico') ? 'no nicos allowed' : true),
-              noNick: (value) => (value.includes('nick') ? 'no nicks allowed' : true),
-            },
-          })}
-          placeholder='First Name'
-        />
-        <input type='text' {...register('lastName', { required: true })} placeholder='Last Name' />
-        <input
-          type='text'
-          {...register('username', {
-            required: true,
-            minLength: {
-              value: 5,
-              message: 'Username should be longer than 5 characters',
-            },
-          })}
-          placeholder='Username'
-        />
-        <input
-          type='text'
-          {...register('password', { required: 'Password is required' })}
-          placeholder='Password'
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          type='text'
-          {...register('passwordCheck', { required: true })}
-          placeholder='Password Check'
-        />
-        <span>{errors?.passwordCheck?.message}</span>
         <button type='submit'>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
