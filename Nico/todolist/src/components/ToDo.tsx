d22c3c84a1727adc596f6df4b70fdb4f0dffc70f
jihyunglee <jhlee7125@gmail.com>
2023-04-21 01:10:51 +0900

    feat: todo 상태 변화에 따른 카테고리 기능 구현

--- Nico/todolist/src/components/ToDo.tsx
+++ Nico/todolist/src/components/ToDo.tsx
@@ -1,7 +1,34 @@
-import { ITodo } from './atoms';
+import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { ITodo, todoState } from './atoms';
 
-function ToDo({ text }: ITodo) {
-  return <li>{text}</li>;
+function ToDo({ text, category, id }: ITodo) {
+  const setTodos = useSetRecoilState(todoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== 'DOING' && (
+        <button name='DOING' onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== 'TO_DO' && (
+        <button name='TO_DO' onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== 'DONE' && (
+        <button name='DONE' onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
 }
 
 export default ToDo;
