1a56dcd759916c2f3c9c2d57f7864bb1704d1a04
jihyunglee <jhlee7125@gmail.com>
2023-04-21 19:48:53 +0900

    feat: 카테고리 별로 sort 하는 기능 구현

--- Nico/todolist/src/ToDoList.tsx
+++ Nico/todolist/src/ToDoList.tsx
@@ -4,17 +4,30 @@ import { todoState, todoSelector } from './components/atoms';
 import { useRecoilValue } from 'recoil';
 
 function ToDoList() {
-  const todos = useRecoilValue(todoState);
-  const selectorOutput = useRecoilValue(todoSelector);
-  console.log('selectorOutput', selectorOutput);
+  const [TO_DOs, DOINGs, DONEs] = useRecoilValue(todoSelector);
 
   return (
     <div>
       <h1>To Do</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {todos.map((todo) => (
+        {TO_DOs.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {DOINGs.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {DONEs.map((todo) => (
           <ToDo key={todo.id} {...todo} />
         ))}
       </ul>
