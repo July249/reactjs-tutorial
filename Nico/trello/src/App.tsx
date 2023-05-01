7b0d007382e2a5c132e47b2f604e521243d3bbf4
jihyunglee <jhlee7125@gmail.com>
2023-05-01 23:31:17 +0900

    feat: understanding atom and selector's get

--- Nico/trello/src/App.tsx
+++ Nico/trello/src/App.tsx
@@ -1,5 +1,20 @@
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { minutesState, hourSelector } from './atoms';
+
 function App() {
-  return null;
+  const [minutes, setMinutes] = useRecoilState(minutesState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(+event.currentTarget.value);
+
+    setMinutes(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input type='number' value={minutes} onChange={onMinutesChange} placeholder='Minutes' />
+      <input type='number' value={hours} placeholder='Hours' />
+    </div>
+  );
 }
 
 export default App;
--- Nico/trello/src/atoms.tsx
+++ Nico/trello/src/atoms.tsx
@@ -1,1 +1,14 @@
 import { atom, selector } from 'recoil';
+
+export const minutesState = atom({
+  key: 'minutesState',
+  default: 0,
+});
+
+export const hourSelector = selector({
+  key: 'hours',
+  get: ({ get }) => {
+    const minutes = get(minutesState);
+    return minutes / 60;
+  },
+});
